ee27d625e26b5e7e8f265f75b3bd5686ff931535
OHMORIYUSUKE <omoriyusuke@gmail.com>
2023-04-05 01:31:24 +0900

    feat:  formatterを導入

--- src/go.mod
+++ src/go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/cosmtrek/air v1.29.0 // indirect
 	github.com/creack/pty v1.1.9 // indirect
 	github.com/gin-gonic/gin v1.7.2
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	github.com/nsf/gocode v0.0.0-20230322162601-b672b49f3818 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,11 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	"log"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "producer"
+	PASS := "producer_pass"
+	PROTOCOL := "tcp"
+	HOST := "db"
+	PORT := "3306"
+	DBNAME := "idolm@ster"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "(" + HOST + ":" + PORT + ")" + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		db := gormConnect()
+		log.Println(&db)
+			c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
