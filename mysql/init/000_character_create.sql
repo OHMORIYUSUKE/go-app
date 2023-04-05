ab65e95a72eefb9f4b5d7b7b9f0e6e7e8b6c9914
OHMORIYUSUKE <omoriyusuke@gmail.com>
2023-04-05 10:37:12 +0900

    wip

--- mysql/init/000_character_create.sql
+++ mysql/init/000_character_create.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `imas_characters`;
 -- テーブル定義
 CREATE TABLE IF NOT EXISTS `imas_characters`(
        `id` INT NOT NULL AUTO_INCREMENT COMMENT '連番ID',
-       `type` VARCHAR(100) NOT NULL COMMENT "キャラクターの所属",
+       `types` VARCHAR(100) NOT NULL COMMENT "キャラクターの所属",
        `ch_name` VARCHAR(50) NOT NULL COMMENT 'キャラクターのフルネーム',
        `ch_name_ruby` VARCHAR(100) NOT NULL COMMENT 'アイドルのフルネームのよみがな',
        `ch_family_name` VARCHAR(20) NOT NULL COMMENT 'キャラクターの苗字',
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `imas_characters`(
        `ch_birth_day` INT NOT NULL COMMENT 'キャラクターの誕生日',
        `ch_gender` INT NOT NULL COMMENT 'キャラクターの性別 0:男 1:女',
        `is_idol` BOOLEAN NOT NULL COMMENT 'アイドルかどうか',
-       `ch_blood_type` VARCHAR(10) DEFAULT ' ' COMMENT '血液型',
+       `ch_blood_types` VARCHAR(10) DEFAULT ' ' COMMENT '血液型',
        `ch_color` VARCHAR(20) DEFAULT ' '  COMMENT 'キャラクターのイメージカラー',
        `cv_name` VARCHAR(50) COMMENT '声優さんのフルネーム',
        `cv_name_ruby` VARCHAR(100) COMMENT '声優さんのフルネームのよみがな',
--- src/main.go
+++ src/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
-	"log"
-
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+
+type Member struct {
+    id int `gorm:"" json:"userName"`
+	types string `gorm:"" json:"userName"`
+    ch_name string `gorm:"" json:"userName"`
+	ch_name_ruby string `gorm:"" json:"userName"`
+	ch_family_name string `gorm:"" json:"userName"`
+	ch_family_name_ruby string `gorm:"" json:"userName"`
+	ch_first_name string `gorm:"" json:"userName"`
+	ch_first_name_ruby string `gorm:"" json:"userName"`
+	ch_birth_month int64 `gorm:"" json:"userName"`
+	ch_birth_day int64 `gorm:"" json:"userName"`
+	ch_gender int64 `gorm:"" json:"userName"`
+	is_idol int64 `gorm:"" json:"userName"`
+	ch_blood_types string `gorm:"" json:"userName"`
+	ch_color string `gorm:"" json:"userName"`
+	cv_name string `gorm:"" json:"userName"`
+	cv_name_ruby string `gorm:"" json:"userName"`
+	cv_family_name string `gorm:"" json:"userName"`
+	cv_family_name_ruby string `gorm:"" json:"userName"`
+	cv_first_name string `gorm:"" json:"userName"`
+	cv_first_name_ruby string `gorm:"" json:"userName"`
+	cv_birth_month int64 `gorm:"" json:"userName"`
+	cv_birth_day int64 `gorm:"" json:"userName"`
+	cv_gender int64 `gorm:"" json:"userName"`
+	cv_nickname string `gorm:"" json:"userName"`
+}
 
 func gormConnect() *gorm.DB {
 	DBMS := "mysql"
@@ -30,9 +58,20 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		db := gormConnect()
-		log.Println(&db)
-			c.JSON(200, gin.H{
-			"message": "pong",
+		//
+		user1 := Member{}
+		db.Debug().Table("imas_characters").First(&user1)
+		// SELECT * FROM users ORDER BY id LIMIT 1;
+		fmt.Println("first:", user1)
+		//
+		// var members []Member
+		// db.Debug().Table("imas_characters").Select("*").Scan(&members)
+		// for i := 0; i < len(members); i++ {
+		// 	log.Println(i)
+		// 	log.Println(members[i])
+		// }
+		c.JSON(200, gin.H{
+			"message":"a",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
